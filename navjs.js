a9d4432ed54278fe3b16bbc33b8ed21f20e897ce
modem2550 <wikham2550@icloud.com>
2024-11-23 20:38:26 +0700

    Update navjs.js

--- navjs.js
+++ navjs.js
@@ -8,40 +8,33 @@ window.addEventListener('scroll', function() {
 });
 
 function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-
-  // ซ่อนเนื้อหาทั้งหมด
-  for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+  
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+  
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    if (tabName === 'All') {
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "block";
+        }
+    } else {
+        var selectedContent = document.getElementsByClassName(tabName);
+        for (i = 0; i < selectedContent.length; i++) {
+            selectedContent[i].style.display = "block";
+        }
+    }
+  
+    evt.currentTarget.className += " active";
   }
 
-  tablinks = document.getElementsByClassName("tablinks");
-
-  // ลบคลาส "active" จากแท็บทั้งหมด
-  for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // แสดงเนื้อหาที่ตรงกับคลาสที่เลือก
-  if (tabName === 'All') {
-      for (i = 0; i < tabcontent.length; i++) {
-          tabcontent[i].style.display = "block"; // แสดงเนื้อหาทั้งหมด
-      }
-  } else {
-      // แสดงเฉพาะเนื้อหาที่มีคลาสตามที่เลือก
-      var selectedContent = document.getElementsByClassName(tabName);
-      for (i = 0; i < selectedContent.length; i++) {
-          selectedContent[i].style.display = "block"; // แสดงเนื้อหาที่เลือก
-      }
-  }
-
-  evt.currentTarget.className += " active";
-}
-
-window.onload = function() {
-  openTab({ currentTarget: document.querySelector('.tablinks') }, 'All');
-};
-
-window.location.href = "https://www.dubaidxbairport.com";
-document.location = "https://www.dubaidxbairport.com";
+  window.onload = function() {
+    openTab({ currentTarget: document.querySelector('.tablinks') }, 'All');
+  };
