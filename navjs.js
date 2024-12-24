558ddde25bb2781e09ec2432032869e0f4997a20
modem2550 <wikham2550@icloud.com>
2024-12-24 19:15:43 +0700

    Update navjs.js

--- navjs.js
+++ navjs.js
@@ -7,6 +7,31 @@ window.addEventListener('scroll', function() {
     }
 });
 
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header-nav');
+    if (window.scrollY > 50) { 
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -33,8 +58,9 @@ function openTab(evt, tabName) {
     }
   
     evt.currentTarget.className += " active";
-  }
 
-  window.onload = function() {
-    openTab({ currentTarget: document.querySelector('.tablinks') }, 'All');
-  };
+    const unsetElement = document.querySelector('.unset');
+    if (unsetElement) {
+        unsetElement.style.display = "none";
+    }
+  }
